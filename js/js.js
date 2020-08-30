fdd33f4f5974dc8ba12143b228b8534e2f75e4db
HbNitin <58799896+HbNitin@users.noreply.github.com>
2020-08-30 20:41:06 +0530

    yup

---
+++ js/js.js
@@ -0,0 +1,29 @@
+//Dedicated To My Best Friend!!
+
+
+
+alert("Sorry due to lack of time and ideas..i only made a small wishing code😢");
+
+alert("I only made just a little thing and designed the text..sorry");
+window.onload=function display()
+{
+    document.getElementById("content").style.display="none";
+ document.getElementsByTagName("body")[0].style.background="linear-gradient(to right bottom,  #00b5ff, #a5fecb";
+  document.getElementsByTagName("body")[0].style.height="400px";
+ var a=setTimeout(ale,2000);
+ 
+}
+function ale(){
+    alert("Try clicking the heart!!");
+}
+function first()
+{
+ 
+  document.getElementById("first").style.display="none";
+alert("THIS IS THE SURPRISE FOR YOU 😂😂"+"\n\n"+"Many Many Happy Returns Of The Day !!😘😘😘🎂🎂");   
+console.log("Turn on the volume"); document.getElementById("hb").style.display="none";
+    
+document.getElementById("content").style.display="block";
+
+document.getElementsByTagName("body")[0].style.background="#7FFFD4";
+}
